bf1db019159175b18cd9125c06bac7e310d8927b
Rogier Mars <rogier.mars@upinion.com>
2017-05-16 20:48:42 +0200

    first version

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+    for (var i = 1; i < 32; i++) {
+        $('#daypicker select')
+            .append($("<option></option>")
+                .attr("value",i)
+                .text(i));
+    }
+
+    $('#startbutton').click(function() {
+        var day = $('#daypicker select').val();
+        if (day !== '-') {
+            if (Number(day) % 2 === 0) {
+                window.location.href = '/tutorials/code.html';
+            } else {
+                window.location.href = '/tutorials/docker.html';
+            }
+        } else {
+            $('#daypicker').addClass('error');
+        }
+    });
+});
